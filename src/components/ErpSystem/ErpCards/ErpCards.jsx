62e9abe7be637d7d81360e73d6cd2d725c1a0ff2
Islomjonsdev <islomjon@immigrateable.com>
2024-07-13 17:53:23 +0500

    fixed some news

---
+++ src/components/ErpSystem/ErpCards/ErpCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from "./ErpCards.module.scss"
+
+const ErpCards = () => {
+  return (
+    <div className={style.erp_system_item}>ErpCards</div>
+  )
+}
+
+export default ErpCards
--- src/components/ErpSystem/ErpSystem.jsx
+++ src/components/ErpSystem/ErpSystem.jsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
+import erpimg from "../../assets/images/erpImg.png"
 import style from "./ErpSystem.module.scss"
 
 const ErpSystem = () => {
   return (
-    <div>ErpSystem</div>
+    <section className={style.erp_system}>
+      <div className={style.container}>
+          <div className={style.erp_system_blog}>
+              <h1>ERP cистемы</h1>
+              <div className={style.erp_system_wrapper}>
+                  <div className={style.erp_system_left}>
+                      <Image src={erpimg} alt="erpImg"/>
+                  </div>
+                  <div className={style.erp_system_right}>
+                      <p>IT Системы любого уровня сложности в удобные для вас сроки.</p>
+                      <div className={style.erp_system_list}></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </section>
   )
 }
 

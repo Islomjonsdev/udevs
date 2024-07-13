3f8863ede4597a8bb59864c7cc980228ff49ef74
Islomjonsdev <islomjon@immigrateable.com>
2024-07-13 23:13:14 +0500

    fixed cards

--- src/components/ErpSystem/ErpCards/ErpCards.jsx
+++ src/components/ErpSystem/ErpCards/ErpCards.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 import style from "./ErpCards.module.scss"
 
-const ErpCards = () => {
+const ErpCards = ({erpImg, erpText}) => {
   return (
-    <div className={style.erp_system_item}>ErpCards</div>
+    <div className={style.erp_system_item}>
+        <Image src={erpImg} alt="erpImg"/>
+        <p>{erpText}</p>
+    </div>
   )
 }
 
--- src/components/ErpSystem/ErpSystem.jsx
+++ src/components/ErpSystem/ErpSystem.jsx
@@ -1,27 +1,68 @@
-import Image from 'next/image';
-import React from 'react';
-import erpimg from "../../assets/images/erpImg.png"
-import style from "./ErpSystem.module.scss"
+import Image from "next/image";
+import React from "react";
+import erpimg from "../../assets/images/erpImg.png";
+import imageErp1 from "../../assets/svg/image1.svg";
+import imageErp2 from "../../assets/svg/image2.svg";
+import imageErp3 from "../../assets/svg/image3.svg";
+import imageErp4 from "../../assets/svg/image4.svg";
+import imageErp5 from "../../assets/svg/image5.svg";
+import imageErp6 from "../../assets/svg/image6.svg";
+import ErpCards from "./ErpCards/ErpCards";
+
+import style from "./ErpSystem.module.scss";
 
 const ErpSystem = () => {
+  const erpSystemData = [
+    {
+      erpImg: imageErp1,
+      erpText: "CRM",
+    },
+    {
+      erpImg: imageErp2,
+      erpText: "eLearning",
+    },
+    {
+      erpImg: imageErp3,
+      erpText: "E-Commerce",
+    },
+    {
+      erpImg: imageErp4,
+      erpText: "POS",
+    },
+    {
+      erpImg: imageErp5,
+      erpText: "Sms / Email",
+    },
+    {
+      erpImg: imageErp6,
+      erpText: "Warehouse",
+    },
+  ];
   return (
     <section className={style.erp_system}>
       <div className={style.container}>
-          <div className={style.erp_system_blog}>
-              <h1>ERP cистемы</h1>
-              <div className={style.erp_system_wrapper}>
-                  <div className={style.erp_system_left}>
-                      <Image src={erpimg} alt="erpImg"/>
-                  </div>
-                  <div className={style.erp_system_right}>
-                      <p>IT Системы любого уровня сложности в удобные для вас сроки.</p>
-                      <div className={style.erp_system_list}></div>
-                  </div>
+        <div className={style.erp_system_blog}>
+          <h1>ERP cистемы</h1>
+          <div className={style.erp_system_wrapper}>
+            <div className={style.erp_system_left}>
+              <Image src={erpimg} alt="erpImg" />
+            </div>
+            <div className={style.erp_system_right}>
+              <p className={style.erp_system_right_text}>IT Системы любого уровня сложности в удобные для вас сроки.</p>
+              <div className={style.erp_system_list}>
+                {erpSystemData?.map((item, index) => {
+                  const { erpImg, erpText } = item;
+                  return (
+                    <ErpCards key={index} erpImg={erpImg} erpText={erpText} />
+                  );
+                })}
               </div>
+            </div>
           </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ErpSystem
+export default ErpSystem;

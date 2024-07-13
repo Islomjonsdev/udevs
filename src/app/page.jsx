fd6e87cb64ee4e9b4898f4c7cf72e5de21ca1bb6
Islomjonsdev <islomjon@immigrateable.com>
2024-07-13 12:18:56 +0500

    responsive done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import OurServices from "@/components/OurServices/OurServices";
+import Team from "@/components/Team/Team";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <OurServices />
+      <Team />
     </>
   );
 }

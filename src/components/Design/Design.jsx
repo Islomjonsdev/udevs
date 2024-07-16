92d9b110f3b91e68b6328e5dea49afdb3d27daef
Islomjonsdev <islomjon@immigrateable.com>
2024-07-16 15:23:06 +0500

    done responsive

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -7,9 +7,15 @@ import cardImg3 from "../../assets/svg/uiux3.svg";
 import cardImg4 from "../../assets/svg/uiux4.svg";
 import cardImg5 from "../../assets/svg/uiux5.svg";
 import cardImg6 from "../../assets/svg/uiux6.svg";
+import cardImgRight from "../../assets/images/uiuximg.png";
+import figmaIcon from "../../assets/svg/figma.svg";
+import secIcon from "../../assets/svg/sec.svg";
+import lottieIcon from "../../assets/svg/lottie.svg";
+import lllusIcon from "../../assets/svg/lllus.svg";
 import Card from "./Card/Card";
 
 import style from "./Design.module.scss";
+import Image from "next/image";
 
 const Design = () => {
   const cardData = [
@@ -38,6 +44,25 @@ const Design = () => {
       cardText: "Atomic Design",
     },
   ];
+
+  const technologyData = [
+    {
+      techImg: figmaIcon,
+      techText: "Figma",
+    },
+    {
+      techImg: secIcon,
+      techText: "Sketch",
+    },
+    {
+      techImg: lottieIcon,
+      techText: "Lottie",
+    },
+    {
+      techImg: lllusIcon,
+      techText: "Illustrator",
+    },
+  ];
   return (
     <section className={style.design}>
       <div className={style.container}>
@@ -57,6 +82,24 @@ const Design = () => {
                   );
                 })}
               </div>
+              <div className={style.card_bottom}>
+                <h3>Технологии</h3>
+                <div className={style.card_bottom_list}>
+                  {technologyData?.map((card, index) => {
+                    const { techImg, techText } = card;
+                    return (
+                      <div className={style.card_bottom_item} key={index}>
+                        <Image className={style.card_bottom_img} src={techImg} alt="techImg" />
+                        <p className={style.card_bottom_text}>{techText}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+
+            <div className={style.card_right}>
+              <Image src={cardImgRight} alt="cardImgRight" />
             </div>
           </div>
         </div>
